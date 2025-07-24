9341064b7f54375ced9b0515731b375d236af35c
Nabil Abdulbari Abdulqader Al-Harethi <na223bj@student.lnu.se>
2025-07-24 16:10:21 +0200

    adding the project to git

---
+++ app/main.py
@@ -0,0 +1,16 @@
+
+"""
+Time zone API :
+
+This application provides RESTful endpoints for time zones operations including:
+
+- current time retriveal for multiple timezones
+- time zone conversion utilites 
+- business hours checking
+- comprehensive timezone metadata
+
+Author: Nabil Al-Harethi
+
+version 1.0.0
+
+"""
